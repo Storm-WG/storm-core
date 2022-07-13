8b354ebd277b6379f7934ba6717e688e65f9ecb6
Dr Maxim Orlovsky <orlovsky@pandoraprime.ch>
2022-07-14 01:33:15 +0200

    Add todo comment

--- src/p2p.rs
+++ src/p2p.rs
@@ -80,6 +80,7 @@ pub enum Messages {
     #[display("accept({0})")]
     Accept(AppMsg<MesgId>),
 
+    // TODO: Consider using Storm mesgs for this
     /// Announce container.
     #[api(type = 0x0011)]
     #[display("announce_container({0})")]
